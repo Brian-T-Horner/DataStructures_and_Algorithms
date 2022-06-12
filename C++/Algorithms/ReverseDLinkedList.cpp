032653f21638bda042d546fb6fab1fd413e7c8ee
Brian Horner <bhorner@suffolk.edu>
2022-06-11 20:33:25 -0400

    Added reverse doubly linked list

--- C++/Algorithms/ReverseDLinkedList.cpp
+++ C++/Algorithms/ReverseDLinkedList.cpp
@@ -2,6 +2,23 @@
 //
 // Author: Brian Horner
 // Edit History:
-// 6/6/2022 - Initial Version
+// 6/11/2022 - Initial Version
 
-//TODO:: Implement this
+#include "../Data_Structures/Doubly_Linked_List/DoublyLinkedList.h"
+
+
+// Reverses the contents of a doubly linked list current
+template <typename T>
+void listReverse(DoublyLinkedList<T> current){
+    DoublyLinkedList<T> temp;
+    while(!current.empty()){
+        T element = current.front();
+        current.removeFront();
+        temp.addFront(element);
+    }
+    while (!temp.empty()){
+        T element = temp.front();
+        temp.removeFront();
+        current.addBack(element);
+    }
+}
